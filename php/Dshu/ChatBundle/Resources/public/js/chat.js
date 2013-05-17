913d8d2758805f2c6c336f3732e585dac922406e
dshu610 <dshu610@gmail.com>
2013-05-17 00:01:41 -0400

    reorganizing some of the front end stuff for easier reusability

--- php/Dshu/ChatBundle/Resources/public/js/chat.js
+++ php/Dshu/ChatBundle/Resources/public/js/chat.js
@@ -1,10 +1,10 @@
-function sendData(){
+function sendData(chatid,user,message,txtInput){
 	$.post(window.sendURL, {
-		chatid : $("#form_chatid").val(),
-		user : $("#form_user").val(),
-		message : $("#form_message").val()},function(){
-			$("#form_message").val('');
-			updateChat();
+		chatid : chatid,
+		user : user,
+		message : message},function(){
+			$("#" + txtInput).val('');
+			updateChat(chatid,$(".chat_messages"));
 		});
 }
 
@@ -12,31 +12,20 @@ function extractTime(tstr){
 	timestamp = new Date(parseInt(tstr.substring(0,8), 16)*1000);
 	return timestamp.getHours() + ":" + timestamp.getMinutes();
 }
-function updateChat(){
+function updateChat(chatid){
 	$.post(window.getURL, {
-		chatid : $("#form_chatid").val(),
+		chatid : chatid,
 		time : curTime},function(data){
 				var obj = data['messages'];
 				for(var i=0; i<obj.length;i++){
 					json = obj[i];
 					oid = json['id'];
 					
-					var msg = $( "<div id=\"msg\"><span class=\"spanUser\">" + json['user'] + 
-									"</span><span class=\"spanTime\"> [" + extractTime(oid) + "]</span> : " + json['message'] + "</div>");
-					$("#messages").append(msg);
-					$("#messages").animate({scrollTop: '400px'});
+					var msg = $( "<div class=\"chat_msg\"><span class=\"chat_spanUser\">" + json['user'] + 
+									"</span><span class=\"chat_spanTime\"> [" + extractTime(oid) + "]</span> : " + json['message'] + "</div>");
+					$(".chat_messages").append(msg);
+					$(".chat_messages").animate({scrollTop: $(".chat_messages").height()});
 					curTime = (parseInt(oid.substring(0,8),16)+1).toString(16);
 				}
 		},"json");
 }
-$(document).ready(function(){
-	updater = setInterval(updateChat,5000);
-	// prevent enter button from submitting form
-	$("#sendForm").bind('keypress', function(e) {
-		if(e.keyCode == 13){
-			e.preventDefault();
-			sendData();
-		}
-	    
-	});
-});
